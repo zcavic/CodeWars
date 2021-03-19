2d3050925e843eb0973d13b5e84d018222c0b650
Zeljko Cavic <zeljko.cavic@schneider-electric-dms.com>
2021-03-19 15:57:01 +0100

    String to array

---
+++ CodeWars/String/String.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.String
+{
+	public static class String
+	{
+		public static string[] StringToArray(string str) => str.Split(" ");
+	}
+}
