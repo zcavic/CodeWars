ec1bcd02f1e46666201272cdc0d0e62284512a75
Zeljko Cavic <zeljko.cavic@schneider-electric-dms.com>
2021-03-19 16:34:35 +0100

    IsIsogram

--- CodeWars/String/String.cs
+++ CodeWars/String/String.cs
@@ -8,6 +8,13 @@ namespace CodeWars.String
 {
 	public static class String
 	{
-		public static string[] StringToArray(string str) => str.Split(" ");
+		public static string[] StringToArray(string str) => str.Split(' ');
+
+		/// <summary>
+		/// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+		/// </summary>
+		/// <param name="str">String to check</param>
+		/// <returns>True if string is isogram, otherwise false</returns>
+		public static bool IsIsogram(string str) => str.ToLower().ToArray().Distinct().Count() == str.Count();
 	}
 }
