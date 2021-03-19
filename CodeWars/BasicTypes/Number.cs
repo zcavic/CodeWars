d4bf1159e0124ee60f4c1a18cf96affe88777fc6
Zeljko Cavic <zeljko.cavic@schneider-electric-dms.com>
2021-03-19 16:43:49 +0100

    IsTriangle

---
+++ CodeWars/BasicTypes/Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.BasicTypes
+{
+	public static class Number
+	{
+		/// <summary>
+		/// Method accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <param name="c"></param>
+		/// <returns></returns>
+		public static bool IsTriangle(int a, int b, int c) => a + b > c && a + c > b && b + c > a;
+	}
+}
--- CodeWars/BasicTypes/String.cs
+++ CodeWars/BasicTypes/String.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeWars.String
+namespace CodeWars.BasicTypes
 {
 	public static class String
 	{
