e384e32fc8b3bf963d3a113b0a2cad9d2986894a
Zeljko Cavic <zeljko.cavic@schneider-electric-dms.com>
2021-03-19 15:50:49 +0100

    Maximal and minimal number in array

---
+++ CodeWars/Collections/Array.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace CodeWars.Collections
+{
+	public static class Array
+	{
+		/// <summary>
+		/// Find minimal number in array
+		/// </summary>
+		/// <param name="list">Array</param>
+		/// <returns>Minimal number in array</returns>
+		public static int Min(int[] list) => list.Min();
+
+		/// <summary>
+		/// Find maximal number in array
+		/// </summary>
+		/// <param name="list">Array</param>
+		/// <returns>Maximal number in array</returns>
+		public static int Max(int[] list) => list.Max();
+	}
+}
